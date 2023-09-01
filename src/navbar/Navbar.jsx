818c506b4c8a169a0e38c0c9270efbcb8a0313ff
Shehab Waleed <shehabwaleedd@gmail.com>
2023-09-01 08:29:01 +0300

    revamped the navbar and style layout. added new font.

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { AnimatePresence, motion } from 'framer-motion'
-import './Navbar.css'
+import './Navbar.scss'
 import logo from "../assets/minimal-logo-for-coconut-s-tree- (2).svg"
 const Navbar = ({ setNavOpen, navOpen, setIsMobile, setIsTablet, isTablet, isMobile }) => {
     const location = useLocation();
@@ -34,27 +34,35 @@ const Navbar = ({ setNavOpen, navOpen, setIsMobile, setIsTablet, isTablet, isMob
         };
     }, [setIsTablet]);
     return (
-        <motion.nav className={`nav ${navOpen ? 'open' : ''}`} >
-            <div className="nav__container">
+        <motion.nav className={`nav`} >
+            <div className="nav__container containered">
                 <div className="nav__left">
                     <Link className="nav__logo" to="/">
-                        <h1 className='logo__name'>Le Rêvee</h1>
-                        <img src={logo} alt="" className='svg' style={{width: "15rem"}}/>
+                        <h1 className='logo__name'>il Rêve</h1>
+                        <img src={logo} alt="" className='svg' style={{ width: "15rem" }} />
                     </Link>
                 </div>
-                <div className="nav__middle">
-                    <ul className="nav__list">
-                        <motion.li className="nav__item">
-                            <div className="hotel__circle"></div>
-                            <Link className="nav__link" to="/hotel">Hotel</Link>
-                        </motion.li>
-                        <motion.li className="nav__item">
-                            <div className="restaurant__circle"></div>
-                            <Link className="nav__link" to="/restaurant">Restaurant</Link>
-                        </motion.li>
-                    </ul>
-                </div>
-                <a href="#menu" className="nav__right">
+                <div className="nav__right">
+                    <div className="nav__middle_left">
+                        <ul className="nav__list">
+                            <motion.li className="nav__item">
+                                <div className="hotel__circle"></div>
+                                <Link className="nav__link" to="/hotel">Hotel</Link>
+                            </motion.li>
+                            <motion.li className="nav__item">
+                                <div className="restaurant__circle"></div>
+                                <Link className="nav__link" to="/restaurant">Restaurant</Link>
+                            </motion.li>
+                            <motion.li className="nav__item">
+                                <Link className="nav__link" to="/about">About</Link>
+                            </motion.li>
+                            <motion.li className="nav__item">
+                                <Link className="nav__link" to="/contact">Contact</Link>
+                            </motion.li>
+                        </ul>
+                    </div>
+
+                    {/* <a href="#menu" className="nav__right">
                     <svg className={`ham hamRotate ham1 ${navOpen ? 'active' : ''}`} viewBox="0 0 100 100" width="100" height="50" onClick={() => setNavOpen(!navOpen)}>
                         <path
                             className={`${navOpen ? "line open" : "line"} top`}
@@ -66,7 +74,8 @@ const Navbar = ({ setNavOpen, navOpen, setIsMobile, setIsTablet, isTablet, isMob
                             className={`${navOpen ? "line open" : "line"} bottom`}
                             d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" />
                     </svg>
-                </a>
+                </a> */}
+                </div>
             </div>
         </motion.nav>
     )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,10 @@
 
 import React from 'react'
 import './Home.css'
-import { LuHotel } from "react-icons/lu"
-import { BsArrowUpRight } from "react-icons/bs"
-import {IoRestaurantOutline} from "react-icons/io5"
-import {PiCookingPot} from "react-icons/pi"
-import {IoColorPaletteOutline} from "react-icons/io5"
-import {GiMeditation} from "react-icons/gi"
 const Home = () => {
     return (
         <section className='home'>
-            <div className="home__container">
+            <div className="home__container containered">
                 <div className="home__left">
                     <div className="home__left-container">
                         <h1 className="home__title">Le Rêve Group</h1>
@@ -19,55 +13,6 @@ const Home = () => {
                             <div className="left-lower-btns">
                                 <button className="left-lower-btn">Book Now</button>
                                 <button className="learn-more-btn">Learn More</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="home__right">
-                    <div className="home__right-cards">
-                        <div className="home__right-card hotel">
-                            <div className="right_card-icon">
-                                <LuHotel style={{ fontSize: "3rem", borderRadius: "2rem", padding: "0.8rem", color: "var(--container-color)", backgroundColor: "var(--nav-overlay-color)" }} />
-                            </div>
-                            <div className="right_card-title">
-                                <h1 style={{ color: "var(--container-color)" }}>Hotel</h1>
-                                <BsArrowUpRight style={{ fontSize: "3.5rem", border: "1px solid var(--container-color)", borderRadius: "2rem", padding: "1rem", color: "var(--container-color)" }} />
-                            </div>
-                        </div>
-                        <div className="home__right-card restaurant">
-                            <div className="right_card-icon">
-                                <IoRestaurantOutline style={{ fontSize: "3rem", borderRadius: "2rem", padding: "0.8rem", color: "var(--container-color)", backgroundColor: "var(--nav-overlay-color)" }} />
-                            </div>
-                            <div className="right_card-title">
-                                <h1 style={{ color: "var(--container-color)" }}>Restaurant</h1>
-                                <BsArrowUpRight style={{ fontSize: "3.5rem", border: "1px solid var(--container-color)", borderRadius: "2rem", padding: "1rem", color: "var(--container-color)" }} />
-                            </div>
-                        </div>
-                        <div className="home__right-card cooking">
-                            <div className="right_card-icon">
-                                <PiCookingPot style={{ fontSize: "3rem",borderRadius: "2rem", padding: "0.8rem", color: "var(--container-color)", backgroundColor: "var(--nav-overlay-color)" }} />
-                            </div>
-                            <div className="right_card-title">
-                                <h1 style={{ color: "var(--container-color)" }}>Cooking Class</h1>
-                                <BsArrowUpRight style={{ fontSize: "3.5rem", border: "1px solid var(--container-color)", borderRadius: "2rem", padding: "1rem", color: "var(--container-color)" }} />
-                            </div>
-                        </div>
-                        <div className="home__right-card painting">
-                            <div className="right_card-icon">
-                                <IoColorPaletteOutline style={{ fontSize: "3rem", borderRadius: "2rem", padding: "0.8rem", color: "var(--container-color)", backgroundColor: "var(--nav-overlay-color)" }} />
-                            </div>
-                            <div className="right_card-title">
-                                <h1 style={{ color: "var(--container-color)" }}>Painting Class</h1>
-                                <BsArrowUpRight style={{ fontSize: "3.5rem", border: "1px solid var(--container-color)", borderRadius: "2rem", padding: "1rem", color: "var(--container-color)" }} />
-                            </div>
-                        </div>
-                        <div className="home__right-card yoga">
-                            <div className="right_card-icon">
-                                <GiMeditation style={{ fontSize: "3rem", borderRadius: "2rem", padding: "0.8rem", color: "var(--container-color)", backgroundColor: "var(--nav-overlay-color)" }} />
-                            </div>
-                            <div className="right_card-title">
-                                <h1 style={{ color: "var(--container-color)" }}>Yoga</h1>
-                                <BsArrowUpRight style={{ fontSize: "3.5rem", border: "1px solid var(--container-color)", borderRadius: "2rem", padding: "1rem",  color: "var(--container-color)"}} />
                             </div>
                         </div>
                     </div>
