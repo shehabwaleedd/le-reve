4d2083862ef2e095a37311497704dd0bdc3f3465
Shehab Waleed <shehabwaleedd@gmail.com>
2023-09-01 08:42:52 +0300

    removed achievments.

--- src/pages/home/SiteHome.jsx
+++ src/pages/home/SiteHome.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Home from './Home'
-import Achievments from './achievments/Achievments'
 
 const SiteHome = () => {
     return (
         <main className='main'>
             <Home />
-            <Achievments />
         </main>
     )
 }
