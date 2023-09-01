60abfc6bb57c9ce4423f65c5efdfcde1b418869c
Shehab Waleed <shehabwaleedd@gmail.com>
2023-09-02 01:41:55 +0300

    added indication that hotel under construction

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./Hotel.scss"
+import { Link } from 'react-router-dom'
 
 const Hotel = () => {
     return (
         <div className='hotel'>
             <h1>Page Under Construction</h1>
+            <p>
+                We are working on this page. Please come back later.
+            </p>
+            <Link to="/">
+                <span>Go Back</span>
+            </Link>
         </div>
     )
 }
--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -3,6 +3,7 @@ import { useRoutes } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import NotFound from '../pages/notFound/NotFound';
 import SiteHome from '../pages/home/SiteHome';
+import Hotel from '../pages/hotel/Hotel';
 const Routers = ({ navOpen }) => {
     const location = useLocation();
     const routeConfig = [
@@ -10,7 +11,7 @@ const Routers = ({ navOpen }) => {
             path: '/',
             element: <SiteHome key={location.pathname} navOpen={navOpen} />
         },
-
+        { path: '/hotel', element: <Hotel /> },
         { path: '*', element: <NotFound /> },
     ];
 
