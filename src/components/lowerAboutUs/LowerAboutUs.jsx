207d97dae45f158762c50851284b4b81a5bb34b0
Shehab Waleed <shehabwaleedd@gmail.com>
2023-09-01 10:12:51 +0300

    fininshed thte lowerAboutUs seciton.

---
+++ src/components/lowerAboutUs/LowerAboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import lower from "../../assets/lower.jpg"
+import "./LowerAboutUs.scss"
+
+const LowerAboutUs = () => {
+    return (
+        <div className='lowerAboutUs containered'>
+            <img src={lower} alt="" />
+        </div>
+    )
+}
+
+export default LowerAboutUs
--- src/pages/home/SiteHome.jsx
+++ src/pages/home/SiteHome.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './Home'
-import AboutUs from '../../components/loading/aboutus/AboutUs'
+import AboutUs from '../../components/aboutus/AboutUs'
+import LowerAboutUs from '../../components/lowerAboutUs/LowerAboutUs'
 
 const SiteHome = () => {
     return (
         <main className='main'>
             <Home />
             <AboutUs />
+            <LowerAboutUs />
         </main>
     )
 }
