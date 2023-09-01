f3a0e33b33d266e7a33cff10823f3b861120db36
Shehab Waleed <shehabwaleedd@gmail.com>
2023-09-01 21:04:40 +0300

    minor design fixes.

--- src/components/contactUpper/ContactUpper.jsx
+++ src/components/contactUpper/ContactUpper.jsx
@@ -6,9 +6,9 @@ const ContactUpper = () => {
         <div className='contactUpper'>
             <div className="contactUpper__container containered">
                 <div className="contactUpper__left">
-                    <h1>
+                    <h2>
                         We Are Always Glad To See You
-                    </h1>
+                    </h2>
                 </div>
                 <div className="contactUpper__right">
                     <span> If you have any urgent question, personal wishes that you do not want to leave online, you can leave us your details and we will contact you to discuss all the details </span>
