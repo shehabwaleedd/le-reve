7ffecef34d15b19902670d556cfd243389750cc5
Shehab Waleed <shehabwaleedd@gmail.com>
2023-09-02 06:42:42 +0300

    added scroll to the top.

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Breakfast from './menuComponents/breakfast/Breakfast'
 import MainCourse from './menuComponents/mainCourse/MainCourse'
 import Appetizers from './menuComponents/appetizers/Appetizers'
 import Salads from './menuComponents/salads/Salads'
 
 const Menu = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
     return (
         <section>
             <Breakfast />
