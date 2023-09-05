25c192c87bd589be7c52a17c15313590a345b944
Shehab Waleed <shehabwaleedd@gmail.com>
2023-09-05 13:49:01 +0300

    added delete confirmation function for better ux and safety as well.

--- src/pages/admin/adminComponents/adminSection/AdminSection.jsx
+++ src/pages/admin/adminComponents/adminSection/AdminSection.jsx
@@ -1,12 +1,42 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./AdminSection.scss"
 import { Link } from 'react-router-dom'
 import { LuEdit2 } from 'react-icons/lu';
+import { AnimatePresence, motion } from 'framer-motion';
 
 
 const AdminSection = ({ sectionData, onDeleteItem, section }) => {
+    const [deleteConfirmation, setDeleteConfirmation] = useState(false);
+    const [itemToDelete, setItemToDelete] = useState(null);
 
-    useEffect (() => {
+    const handleDeleteConfirmation = (item) => {
+        setItemToDelete(item);
+        setDeleteConfirmation(true);
+    };
+
+    const handleDeleteCancel = () => {
+        setDeleteConfirmation(false);
+        setItemToDelete(null);
+    };
+
+    const handleDeleteItem = () => {
+        onDeleteItem(itemToDelete.id);
+        setDeleteConfirmation(false);
+        setItemToDelete(null);
+    };
+
+    if (deleteConfirmation) {
+        document.body.classList.add('no-scroll');
+    } else {
+        document.body.classList.remove('no-scroll');
+    }
+
+
+
+
+
+
+    useEffect(() => {
         console.log(sectionData)
     }, [])
     return (
@@ -15,14 +45,35 @@ const AdminSection = ({ sectionData, onDeleteItem, section }) => {
                 <h1>{section}</h1>
                 <div className="adminSection__container__content">
                     {sectionData.map((item) => (
-                        <div className="adminSection__container__content__item"  key={item.id}>
+                        <div className="adminSection__container__content__item" key={item.id}>
                             <div className="content__item_upper">
                                 <h3>{item.name}</h3>
                                 <div className="item__upper_lower">
-                                    <button onClick={() => onDeleteItem(item.id)} className='delete_btn'>X</button>
+                                    <button onClick={() => handleDeleteConfirmation(item)} className='delete_btn'>X</button>
                                     <Link to={`/edit/${section}/${item.id}`} className='edit_btn'><LuEdit2 /></Link>
                                 </div>
                             </div>
+                                <AnimatePresence mode='wait'>
+                                    {deleteConfirmation && itemToDelete?.id === item.id && (
+                                        <motion.div
+                                            key="delete-confirmation"
+                                            className="deleteConfirmation"
+                                            initial={{ opacity: 0 }}
+                                            animate={{ opacity: 1 }}
+                                            exit={{ opacity: 0 }}
+                                        >
+                                            <p>Are you sure you want to delete this item?</p>
+                                            <div className="deleteConfirmation__btns">
+                                                <button onClick={handleDeleteItem} className="deleteConfirmation__btns__yes">
+                                                    Yes
+                                                </button>
+                                                <button onClick={handleDeleteCancel} className="deleteConfirmation__btns__no">
+                                                    No
+                                                </button>
+                                            </div>
+                                        </motion.div>
+                                    )}
+                                </AnimatePresence>
                             <span>{item.description}</span>
                             <span>${item.price}</span>
                         </div>
