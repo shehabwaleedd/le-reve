d848365ae45fde910bd3194b2aa71993e96029ed
Shehab Waleed <shehabwaleedd@gmail.com>
2023-09-07 14:42:52 +0300

    fixed the height of marquee. added scroll to top once being loaded to about and contact pages.

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AboutUs from '../../components/aboutus/AboutUs'
 import LowerAboutUs from '../../components/lowerAboutUs/LowerAboutUs'
 import AboutBoss from './aboutComponents/aboutBoss/AboutBoss'
@@ -10,6 +10,9 @@ import AboutCrewLower from '../../components/aboutCrewLower/AboutCrewLower'
 import Marquee from '../../components/marquee/Marquee'
 
 const About = () => {
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }, []);
     return (
         <section className='about'>
             <div className="about__container">
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ContactUpper from '../../components/contactUpper/ContactUpper'
 import ContactLower from '../../components/contactLower/ContactLower'
 import ContactsLower from '../../components/contactsLower/ContactsLower'
 import ContactsUpper from '../../components/contactsUpper/ContactsUpper'
 
 const Contact = () => {
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }, []);
     return (
         <section className='contact'>
             <div className="contact__container">
